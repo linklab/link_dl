1e423506160149121ee6ceebc9466e2b6d4ccb3e
Youn-Hee Han <yh21.han@gmail.com>
2023-08-10 18:06:26 +0900

    ...

---
+++ _04_learning_and_autograd/j_autograd_new_function_4.py
@@ -0,0 +1,22 @@
+import torch
+from torch import optim, nn
+
+x = torch.ones(3)
+w = torch.ones(3, requires_grad=True)
+sigmoid = nn.Sigmoid()
+y = torch.tensor([1.1, 1.1, 1.1])
+
+EPOCHS = 1200
+LEARNING_RATE = 1e-2
+optimizer = optim.SGD([w], lr=LEARNING_RATE)
+
+
+for t in range(EPOCHS):
+    y_pred = sigmoid(x * w)
+    loss = (y_pred - y).pow(2).mean()
+
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    print(w)
