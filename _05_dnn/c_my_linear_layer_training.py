32c326af202dace75ed289eebce1557f4745ec87
Youn-Hee Han <yh21.han@gmail.com>
2023-08-26 09:32:03 +0900

    ...

--- _05_dnn/c_my_linear_layer_training.py
+++ _05_dnn/c_my_linear_layer_training.py
@@ -1,75 +1,96 @@
 import torch
 from torch import nn, optim
 
-t_c = [0.5,  14.0, 15.0, 28.0, 11.0,  8.0,  3.0, -4.0,  6.0, 13.0, 21.0]
-t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
 
-t_c = torch.tensor(t_c).unsqueeze(1)
-t_u = torch.tensor(t_u).unsqueeze(1)
+def get_data():
+  data_in = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0]
+  data_out = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4]
 
-print(t_c.shape, t_u.shape)
+  # Adds the extra dimension at axis 1
+  data_in = torch.tensor(data_in).unsqueeze(1)
+  data_out = torch.tensor(data_out).unsqueeze(1)
 
-n_samples = t_u.shape[0]
-n_val = int(0.2 * n_samples)
+  print(data_in.shape, data_out.shape)
 
-shuffled_indices = torch.randperm(n_samples)
+  n_samples = data_out.shape[0]
+  n_val = int(0.2 * n_samples)
 
-train_indices = shuffled_indices[:-n_val]
-val_indices = shuffled_indices[-n_val:]
+  shuffled_indices = torch.randperm(n_samples)
+  print(shuffled_indices)
 
-print(train_indices, val_indices)
+  train_indices = shuffled_indices[:-n_val]
+  val_indices = shuffled_indices[-n_val:]
 
-t_u_train = t_u[train_indices]
-t_c_train = t_c[train_indices]
+  print(train_indices, val_indices)
 
-t_u_val = t_u[val_indices]
-t_c_val = t_c[val_indices]
+  data_in_train = data_in[train_indices]
+  data_out_train = data_out[train_indices]
 
-t_un_train = 0.1 * t_u_train
-t_un_val = 0.1 * t_u_val
+  data_in_val = data_in[val_indices]
+  data_out_val = data_out[val_indices]
+
+  data_out_train = 0.1 * data_out_train
+  data_out_val = 0.1 * data_out_val
+
+  return data_in_train, data_out_train, data_in_val, data_out_val
 
 
-linear_model = nn.Linear(1, 1)
-optimizer = optim.SGD(linear_model.parameters(),lr=1e-2)
+def training_loop(model, optimizer, data_out_train, data_out_val, data_in_train, data_in_val):
+  n_epochs = 3000
+  loss_fn = nn.MSELoss()  # Use a built-in loss function
 
-print()
-print(linear_model.parameters())
-
-for idx, parameter in enumerate(linear_model.parameters()):
-  print(idx, parameter.data, parameter.data.shape, parameter.requires_grad)
-
-print(linear_model.weight)
-print(linear_model.bias)
-
-print()
-
-def training_loop(n_epochs, optimizer, model, loss_fn, t_u_train, t_u_val, t_c_train, t_c_val):
   for epoch in range(1, n_epochs + 1):
-      t_p_train = model(t_u_train)
-      loss_train = loss_fn(t_p_train, t_c_train)
+    t_p_train = model(data_out_train)
+    loss_train = loss_fn(t_p_train, data_in_train)
 
-      t_p_val = model(t_u_val)
-      loss_val = loss_fn(t_p_val, t_c_val)
+    t_p_val = model(data_out_val)
+    loss_val = loss_fn(t_p_val, data_in_val)
 
-      optimizer.zero_grad()
-      loss_train.backward()
-      optimizer.step()
+    optimizer.zero_grad()
+    loss_train.backward()
+    optimizer.step()
 
-      if epoch == 1 or epoch % 1000 == 0:
-        print(
-          f"Epoch {epoch}, "
-          f"Training loss {loss_train.item():.4f},"
-          f"Validation loss {loss_val.item():.4f}"
-        )
+    if epoch == 1 or epoch % 100 == 0:
+      print(
+        f"Epoch {epoch}, "
+        f"Training loss {loss_train.item():.4f},"
+        f"Validation loss {loss_val.item():.4f}"
+      )
 
 
-training_loop(
-    n_epochs=3000,
-    optimizer=optimizer,
+def get_model_and_optimizer():
+  linear_model = nn.Linear(1, 1)
+
+  print(linear_model.weight)
+  print(linear_model.bias)
+
+  print("#" * 50, 2)
+
+  optimizer = optim.SGD(linear_model.parameters(), lr=1e-2)
+
+  print(linear_model.parameters())
+
+  for idx, parameter in enumerate(linear_model.parameters()):
+    print(idx, parameter.data, parameter.data.shape, parameter.requires_grad)
+
+  return linear_model, optimizer
+
+
+if __name__ == "__main__":
+  data_in_train, data_out_train, data_in_val, data_out_val = get_data()
+  print(data_in_train.shape, data_out_train.shape, data_in_val.shape, data_out_val.shape)
+
+  print("#" * 50, 1)
+
+  linear_model, optimizer = get_model_and_optimizer()
+
+  print("#" * 50, 3)
+
+  training_loop(
     model=linear_model,
-    loss_fn=nn.MSELoss(),
-    t_u_train=t_un_train,
-    t_u_val=t_un_val,
-    t_c_train=t_c_train,
-    t_c_val=t_c_val
-)
+    optimizer=optimizer,
+    data_in_train=data_in_train,
+    data_out_train=data_out_train,
+    data_out_val=data_out_val,
+    data_in_val=data_in_val
+  )
--- _05_dnn/d_my_model.py
+++ _05_dnn/d_my_model.py
@@ -2,16 +2,16 @@ import torch
 from torch import nn
 
 
-class MyModel(nn.Module):
-  def __init__(self, n_input, n_unit1, n_output):
+class MyFirstModel(nn.Module):
+  def __init__(self, n_input, n_hidden_unit, n_output):
     super().__init__()
 
     # Inputs to 1st hidden linear layer
-    self.hidden = nn.Linear(n_input, n_unit1)
+    self.hidden = nn.Linear(n_input, n_hidden_unit)
     self.sigmoid = nn.Sigmoid()
 
     # Output layer
-    self.output = nn.Linear(n_unit1, n_output)
+    self.output = nn.Linear(n_hidden_unit, n_output)
     self.softmax = nn.Softmax(dim=1)
 
   def forward(self, x):
@@ -19,23 +19,67 @@ class MyModel(nn.Module):
     x = self.sigmoid(x)
     x = self.output(x)
     x = self.softmax(x)
+    return x
+
+
+class MySecondModel(nn.Module):
+  def __init__(self, n_input, n_hidden_unit, n_output):
+    super().__init__()
+
+    self.model = nn.Sequential(
+      nn.Linear(n_input, n_hidden_unit),
+      nn.Sigmoid(),
+      nn.Linear(n_hidden_unit, n_output),
+      nn.Softmax(dim=1)
+    )
+
+  def forward(self, x):
+    x = self.model(x)
+    return x
+
+
+class MyThirdModel(nn.Module):
+  def __init__(self, n_input, n_hidden_unit, n_output):
+    super().__init__()
+
+    hidden = nn.Linear(n_input, n_hidden_unit)
+    sigmoid = nn.Sigmoid()
+    output = nn.Linear(n_hidden_unit, n_output)
+    softmax = nn.Softmax(dim=1)
+
+    modules = [hidden, sigmoid, output, softmax]
+    self.module_list = nn.ModuleList(modules)
+
+  def forward(self, x):
+    for f in self.module_list:
+      x = f(x)
+    return x
 
 
 if __name__ == "__main__":
-  my_linear = MyLinear(in_features=4, out_features=3)
-  sample_input = torch.randn(4)
-  output = my_linear(sample_input)
+  batch_input = torch.randn(4, 10)
 
-  print("input.shape:", sample_input.shape)
-  print("my_linear.weight.shape:", my_linear.weight.shape)
-  print("my_linear.bias.shape:", my_linear.bias.shape)
-  print("output.shape:", output.shape)
-  print(output)
+  my_first_model = MyFirstModel(n_input=10, n_hidden_unit=20, n_output=3)
+  for name, param in my_first_model.named_parameters():
+    print(name, param.shape)
 
-  print("#" * 50)
+  first_output = my_first_model(batch_input)
+  print(first_output)
 
-  my_linear_2 = nn.Linear(in_features=4, out_features=3)
-  output_2 = my_linear(sample_input)
-  print(output_2)
+  print("#" * 50, 1)
 
-  assert output.equal(output_2)
+  my_second_model = MySecondModel(n_input=10, n_hidden_unit=20, n_output=3)
+  for name, param in my_second_model.named_parameters():
+    print(name, param.shape)
+
+  second_output = my_second_model(batch_input)
+  print(second_output)
+
+  print("#" * 50, 2)
+
+  my_third_model = MyThirdModel(n_input=10, n_hidden_unit=20, n_output=3)
+  for name, param in my_third_model.named_parameters():
+    print(name, param.shape)
+
+  third_output = my_third_model(batch_input)
+  print(third_output)
