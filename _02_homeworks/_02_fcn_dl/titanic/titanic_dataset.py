8e197a9fb2ae8e7b6e461acd92e679d76db66383
Youn-Hee Han <yh21.han@gmail.com>
2023-09-24 16:00:33 +0900

    ...

--- _02_homeworks/_02_fcn_dl/titanic/titanic_dataset.py
+++ _02_homeworks/_02_fcn_dl/titanic/titanic_dataset.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader, random_split
@@ -42,6 +41,7 @@ class TitanicTestDataset(Dataset):
     )
     return str
 
+
 def get_preprocessed_dataset():
     CURRENT_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -53,57 +53,17 @@ def get_preprocessed_dataset():
 
     all_df = pd.concat([train_df, test_df], sort=False)
 
-    # Pclass별 Fare 평균값을 사용하여 Fare 결측치 메우기
-    Fare_mean = all_df[["Pclass", "Fare"]].groupby("Pclass").mean().reset_index()
-    Fare_mean.columns = ["Pclass", "Fare_mean"]
-    all_df = pd.merge(all_df, Fare_mean, on="Pclass", how="left")
-    all_df.loc[(all_df["Fare"].isnull()), "Fare"] = all_df["Fare_mean"]
+    all_df = get_preprocessed_dataset_1(all_df)
 
-    # name을 세 개의 컬럼으로 분리하여 다시 all_df에 합침
-    name_df = all_df["Name"].str.split("[,.]", n=2, expand=True)
-    name_df.columns = ["family_name", "honorific", "name"]
-    name_df["family_name"] = name_df["family_name"].str.strip()
-    name_df["honorific"] = name_df["honorific"].str.strip()
-    name_df["name"] = name_df["name"].str.strip()
-    all_df = pd.concat([all_df, name_df], axis=1)
+    all_df = get_preprocessed_dataset_2(all_df)
 
-    # honorific별 Age 평균값을 사용하여 Age 결측치 메우기
-    honorific_age_mean = all_df[["honorific", "Age"]].groupby("honorific").median().round().reset_index()
-    honorific_age_mean.columns = ["honorific", "honorific_age_mean", ]
-    all_df = pd.merge(all_df, honorific_age_mean, on="honorific", how="left")
-    all_df.loc[(all_df["Age"].isnull()), "Age"] = all_df["honorific_age_mean"]
-    all_df = all_df.drop(["honorific_age_mean"], axis=1)
+    all_df = get_preprocessed_dataset_3(all_df)
 
-    # 가족수(family_num) 컬럼 새롭게 추가
-    all_df["family_num"] = all_df["Parch"] + all_df["SibSp"]
+    all_df = get_preprocessed_dataset_4(all_df)
 
-    # 혼자탑승(alone) 컬럼 새롭게 추가
-    all_df.loc[all_df["family_num"] == 0, "alone"] = 1
-    all_df["alone"].fillna(0, inplace=True)
+    all_df = get_preprocessed_dataset_5(all_df)
 
-    # 학습에 불필요한 컬럼 제거
-    all_df = all_df.drop(["PassengerId", "Name", "family_name", "name", "Ticket", "Cabin"], axis=1)
-
-    # honorific 값 개수 줄이기
-    all_df.loc[
-    ~(
-            (all_df["honorific"] == "Mr") |
-            (all_df["honorific"] == "Miss") |
-            (all_df["honorific"] == "Mrs") |
-            (all_df["honorific"] == "Master")
-    ),
-    "honorific"
-    ] = "other"
-    all_df["Embarked"].fillna("missing", inplace=True)
-
-    # 카테고리 변수를 LabelEncoder를 사용하여 수치값으로 변경하기
-    category_features = all_df.columns[all_df.dtypes == "object"]
-    from sklearn.preprocessing import LabelEncoder
-    for category_feature in category_features:
-        le = LabelEncoder()
-        if all_df[category_feature].dtypes == "object":
-          le = le.fit(all_df[category_feature])
-          all_df[category_feature] = le.transform(all_df[category_feature])
+    all_df = get_preprocessed_dataset_6(all_df)
 
     train_X = all_df[~all_df["Survived"].isnull()].drop("Survived", axis=1).reset_index(drop=True)
     train_y = train_df["Survived"]
@@ -117,6 +77,111 @@ def get_preprocessed_dataset():
     #print(test_dataset)
 
     return train_dataset, validation_dataset, test_dataset
+
+
+def get_preprocessed_dataset_1(all_df):
+    # Pclass별 Fare 평균값을 사용하여 Fare 결측치 메우기
+    Fare_mean = all_df[["Pclass", "Fare"]].groupby("Pclass").mean().reset_index()
+    Fare_mean.columns = ["Pclass", "Fare_mean"]
+    all_df = pd.merge(all_df, Fare_mean, on="Pclass", how="left")
+    all_df.loc[(all_df["Fare"].isnull()), "Fare"] = all_df["Fare_mean"]
+
+    return all_df
+
+
+def get_preprocessed_dataset_2(all_df):
+    # name을 세 개의 컬럼으로 분리하여 다시 all_df에 합침
+    name_df = all_df["Name"].str.split("[,.]", n=2, expand=True)
+    name_df.columns = ["family_name", "honorific", "name"]
+    name_df["family_name"] = name_df["family_name"].str.strip()
+    name_df["honorific"] = name_df["honorific"].str.strip()
+    name_df["name"] = name_df["name"].str.strip()
+    all_df = pd.concat([all_df, name_df], axis=1)
+
+    return all_df
+
+
+def get_preprocessed_dataset_3(all_df):
+    # honorific별 Age 평균값을 사용하여 Age 결측치 메우기
+    honorific_age_mean = all_df[["honorific", "Age"]].groupby("honorific").median().round().reset_index()
+    honorific_age_mean.columns = ["honorific", "honorific_age_mean", ]
+    all_df = pd.merge(all_df, honorific_age_mean, on="honorific", how="left")
+    all_df.loc[(all_df["Age"].isnull()), "Age"] = all_df["honorific_age_mean"]
+    all_df = all_df.drop(["honorific_age_mean"], axis=1)
+
+    return all_df
+
+
+def get_preprocessed_dataset_4(all_df):
+    # 가족수(family_num) 컬럼 새롭게 추가
+    all_df["family_num"] = all_df["Parch"] + all_df["SibSp"]
+
+    # 혼자탑승(alone) 컬럼 새롭게 추가
+    all_df.loc[all_df["family_num"] == 0, "alone"] = 1
+    all_df["alone"].fillna(0, inplace=True)
+
+    # 학습에 불필요한 컬럼 제거
+    all_df = all_df.drop(["PassengerId", "Name", "family_name", "name", "Ticket", "Cabin"], axis=1)
+
+    return all_df
+
+
+def get_preprocessed_dataset_5(all_df):
+    # honorific 값 개수 줄이기
+    all_df.loc[
+    ~(
+            (all_df["honorific"] == "Mr") |
+            (all_df["honorific"] == "Miss") |
+            (all_df["honorific"] == "Mrs") |
+            (all_df["honorific"] == "Master")
+    ),
+    "honorific"
+    ] = "other"
+    all_df["Embarked"].fillna("missing", inplace=True)
+
+    return all_df
+
+
+def get_preprocessed_dataset_6(all_df):
+    # 카테고리 변수를 LabelEncoder를 사용하여 수치값으로 변경하기
+    category_features = all_df.columns[all_df.dtypes == "object"]
+    from sklearn.preprocessing import LabelEncoder
+    for category_feature in category_features:
+        le = LabelEncoder()
+        if all_df[category_feature].dtypes == "object":
+          le = le.fit(all_df[category_feature])
+          all_df[category_feature] = le.transform(all_df[category_feature])
+
+    return all_df
+
+
+from torch import nn
+class MyModel(nn.Module):
+  def __init__(self, n_input, n_output):
+    super().__init__()
+
+    self.model = nn.Sequential(
+      nn.Linear(n_input, 30),
+      nn.ReLU(),
+      nn.Linear(30, 30),
+      nn.ReLU(),
+      nn.Linear(30, n_output),
+    )
+
+  def forward(self, x):
+    x = self.model(x)
+    return x
+
+
+def test(test_data_loader):
+  print("[TEST]")
+  batch = next(iter(test_data_loader))
+  print("{0}".format(batch['input'].shape))
+  my_model = MyModel(n_input=11, n_output=2)
+  output_batch = my_model(batch['input'])
+  prediction_batch = torch.argmax(output_batch, dim=1)
+  for idx, prediction in enumerate(prediction_batch, start=892):
+      print(idx, prediction.item())
 
 
 if __name__ == "__main__":
@@ -144,6 +209,6 @@ if __name__ == "__main__":
   for idx, batch in enumerate(validation_data_loader):
     print("{0} - {1}: {2}".format(idx, batch['input'].shape, batch['target'].shape))
 
-  print("[TEST]")
-  batch = next(iter(test_data_loader))
-  print("{0}".format(batch['input'].shape))
+  print("#" * 50, 3)
+
+  test(test_data_loader)
