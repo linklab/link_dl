29520365e821169668fa2c51a42845a2eeec56b8
YHHNA_mac_mini <yh21.han@gmail.com>
2023-11-20 21:39:18 +0900

    ...

--- _01_code/_10_rnn/b_rnn.py
+++ _01_code/_10_rnn/b_rnn.py
@@ -1,17 +1,40 @@
 import torch
 from torch import nn
 
-rnn = nn.RNN(input_size=3, hidden_size=4, num_layers=2)
 
-for name, parameter in rnn.named_parameters():
+rnn1 = nn.RNN(input_size=3, hidden_size=4, num_layers=1)
+
+for name, parameter in rnn1.named_parameters():
   print(name, parameter.shape)
-
-print("#" * 100, 1)
 
 # sequence size (L): 6, input size (F): 3
 input = torch.randn(6, 3)
 
-output, hidden_state = rnn(input)
+output, hidden_state = rnn1(input)
+
+for idx, out in enumerate(output):
+  print(idx, out)     # shape: torch.Size([4])
+
+print()
+
+for idx, hidden in enumerate(hidden_state):
+  print(idx, hidden)  # shape: torch.Size([4])
+
+
+print("#" * 100, 1)
+##############################################################################
+
+rnn2 = nn.RNN(input_size=3, hidden_size=4, num_layers=2)
+
+for name, parameter in rnn2.named_parameters():
+  print(name, parameter.shape)
+
+print("#" * 100, 2)
+
+# sequence size (L): 6, input size (F): 3
+input = torch.randn(6, 3)
+
+output, hidden_state = rnn2(input)
 
 for idx, out in enumerate(output):
   print(idx, out)     # shape: torch.Size([4])
