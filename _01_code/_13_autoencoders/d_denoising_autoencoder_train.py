7b1ad7a2fae70c02687a4f45c1ce5881acb552be
Youn-Hee Han <yh21.han@gmail.com>
2023-11-25 23:11:59 +0900

    ...

--- _01_code/_13_autoencoders/d_denoising_autoencoder_train.py
+++ _01_code/_13_autoencoders/d_denoising_autoencoder_train.py
@@ -25,33 +25,65 @@ def get_model(encoded_space_dim=4):
     class Encoder(nn.Module):
         def __init__(self):
             super(Encoder, self).__init__()
-            self.conv1 = nn.Conv2d(1, 8, kernel_size=(3, 3))
-            self.conv2 = nn.Conv2d(8, 16, kernel_size=(3, 3))
-            self.conv3 = nn.Conv2d(16, 32, kernel_size=(3, 3))
-            self.conv4 = nn.Conv2d(32, 64, kernel_size=(3, 3))
+
+            ### Convolutional section
+            self.encoder_cnn = nn.Sequential(
+                nn.Conv2d(1, 8, 3, stride=2, padding=1),
+                nn.ReLU(True),
+                nn.Conv2d(8, 16, 3, stride=2, padding=1),
+                nn.BatchNorm2d(16),
+                nn.ReLU(True),
+                nn.Conv2d(16, 32, 3, stride=2, padding=0),
+                nn.ReLU(True)
+            )
+
+            ### Flatten layer
+            self.flatten = nn.Flatten(start_dim=1)
+
+            ### Linear section
+            self.encoder_lin = nn.Sequential(
+                nn.Linear(3 * 3 * 32, 128),
+                nn.ReLU(True),
+                nn.Linear(128, encoded_space_dim)
+            )
 
         def forward(self, x):
-            x = F.leaky_relu(self.conv1(x))
-            x = F.leaky_relu(self.conv2(x))
-            x = F.leaky_relu(self.conv3(x))
-            x = F.leaky_relu(self.conv4(x))
+            x = self.encoder_cnn(x)
+            x = self.flatten(x)
+            x = self.encoder_lin(x)
             return x
 
     class Decoder(nn.Module):
         def __init__(self):
             super(Decoder, self).__init__()
-            self.conv1 = nn.ConvTranspose2d(64, 32, kernel_size=(3, 3))
-            self.conv2 = nn.ConvTranspose2d(32, 16, kernel_size=(3, 3))
-            self.conv3 = nn.ConvTranspose2d(16, 8, kernel_size=(3, 3))
-            self.conv4 = nn.ConvTranspose2d(8, 1, kernel_size=(3, 3))
-            self.sigmoid_activation = torch.nn.Sigmoid()
+            self.decoder_lin = nn.Sequential(
+                nn.Linear(encoded_space_dim, 128),
+                nn.ReLU(True),
+                nn.Linear(128, 3 * 3 * 32),
+                nn.ReLU(True)
+            )
+
+            self.unflatten = nn.Unflatten(dim=1,
+                                          unflattened_size=(32, 3, 3))
+
+            self.decoder_conv = nn.Sequential(
+                nn.ConvTranspose2d(32, 16, 3,
+                                   stride=2, output_padding=0),
+                nn.BatchNorm2d(16),
+                nn.ReLU(True),
+                nn.ConvTranspose2d(16, 8, 3, stride=2,
+                                   padding=1, output_padding=1),
+                nn.BatchNorm2d(8),
+                nn.ReLU(True),
+                nn.ConvTranspose2d(8, 1, 3, stride=2,
+                                   padding=1, output_padding=1)
+            )
 
         def forward(self, x):
-            x = F.leaky_relu(self.conv1(x))
-            x = F.leaky_relu(self.conv2(x))
-            x = F.leaky_relu(self.conv3(x))
-            x = F.leaky_relu(self.conv4(x))
-            x = self.sigmoid_activation(x)
+            x = self.decoder_lin(x)
+            x = self.unflatten(x)
+            x = self.decoder_conv(x)
+            x = torch.sigmoid(x)
             return x
 
     class Autoencoder(torch.nn.Module):
@@ -109,7 +141,7 @@ def main(args):
         project_name, model, optimizer, train_data_loader, validation_data_loader, f_mnist_transforms,
         run_time_str, wandb, device, CHECKPOINT_FILE_PATH,
         f_mnist_test_images, f_mnist_transforms,
-        denoising=True,
+        denoising=False,
     )
     regression_trainer.train_loop()
 
